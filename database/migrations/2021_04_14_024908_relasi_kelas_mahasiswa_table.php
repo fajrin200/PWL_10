92bf3a3f7a45644e022456a1b0f43280715516c5
fajrin200 <muhammadfajrin412@gmail.com>
2021-04-21 15:13:50 +0700

    Tgs 1 - crud and relasi mahasiswa

---
+++ database/migrations/2021_04_14_024908_relasi_kelas_mahasiswa_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RelasiKelasMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->dropColumn('Kelas');
+            $table->unsignedBigInteger('kelas_id')->nullable();
+            $table->foreign('kelas_id')->references('id')->on('Kelas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->string('Kelas');
+            $table->dropForeign('kelas_id');
+        });
+    }
+}
