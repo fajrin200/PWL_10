405d364f4e57d3a8b98ebba5e401d52c3d9cd388
fajrin200 <muhammadfajrin412@gmail.com>
2021-05-02 11:05:06 +0700

    Tgs 2 - Cetak nilai mahasiswa to PDF

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -1,27 +1,23 @@
 @extends('mahasiswa.layout')
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row justify-content-center align-items-center">
-            <div class="card" style="width: 24rem;">
-                <div class="card-header">
-                    Detail Mahasiswa
-                </div>
-
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->Nim}}</li>
-                        <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->Nama}}</li>
-                        <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->Kelas->nama_kelas}}</li>
-                        <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->Jurusan}}</li>
-                        <li class="list-group-item"><b>No_Handphone: </b>{{$Mahasiswa->No_Handphone}}</li>
-                        <li class="list-group-item"><b>EMail: </b>{{$Mahasiswa->EMail}}</li>
-                        <li class="list-group-item"><b>Tanggal_lahir: </b>{{$Mahasiswa->Tanggal_lahir}}</li>
-                    </ul>
-                </div>
-
-                <a class="btn btn-success mt3" href="{{ route('mahasiswa.index') }}">Kembali</a>
+<div class="container mt-5">
+    <div class="row justify-content-center align-items-center">
+        <div class="card" style="width: 24rem">
+            <div class="card-header">
+                Detail Mahasiswa
             </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <img width="100px" height="100px" src="{{asset('storage/'.$Mahasiswa->foto)}}"></td>
+                    <li class="list-group-item"><b>Nim: </b>{{ $Mahasiswa->nim }}</li>
+                    <li class="list-group-item"><b>Nama: </b>{{ $Mahasiswa->nama }}</li>
+                    <li class="list-group-item"><b>Kelas: </b>{{ $Mahasiswa->kelas->nama_kelas }}</li>
+                    <li class="list-group-item"><b>Jurusan: </b>{{ $Mahasiswa->jurusan }}</li>
+                </ul>
+            </div>
+            <a class="btn btn-success mt-3" href="{{ route('mahasiswa.index') }}">Kembali</a>
         </div>
     </div>
+</div>
 @endsection
