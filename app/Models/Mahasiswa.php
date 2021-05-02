c67f0d7e5aaf6c9dcea39d1ad74a9c2a7bbefc93
fajrin200 <muhammadfajrin412@gmail.com>
2021-05-02 11:04:04 +0700

    Tgs 2 - Cetak nilai mahasiswa to PDF

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,16 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Kelas;
-use App\Models\Matakuliah;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+    use Illuminate\Notifications\Notifiable;
+    use Illuminate\Database\Eloquent\Model; //Model Eloquent
+    use App\Models\Kelas;
+    use App\Models\Matakuliah;
 
 class Mahasiswa extends Model
 {
     use HasFactory;
     public $timestamps = false;
-    protected $table = 'mahasiswa';
+    protected $table = 'mahasiswas';
     protected $fillable = ['nim','nama','kelas','jurusan'];
 
     public function kelas(){
