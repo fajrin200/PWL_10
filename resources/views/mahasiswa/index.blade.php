8ce37fa53d3e23b9c01e42f3dabf759e36e68ead
fajrin200 <muhammadfajrin412@gmail.com>
2021-05-02 11:05:55 +0700

    Tgs 2 - Cetak nilai mahasiswa to PDF

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,60 +1,61 @@
 @extends('mahasiswa.layout')
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left mt-2">
-                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
-            </div>
-
-            <div class="float-right my-2">
-                <a class="btn btn-success" href="{{ route('mahasiswa.create') }}"> Input Mahasiswa</a>
-                </div>
-            <div class="float-left my-2">
-                <p> Cari data Mahasiswa : </p>
-                <form action="{{'/cari'}}" method="GET">
-                    <input type="text" name="cari" placeholder="Cari mahasiswa .." value="{{ old('cari') }}">
-                    <input type="submit" value="cari">
-            </div>
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="text-center mt-2">
+            <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
         </div>
+        <div class="float-right my-2">
+            <a class="btn btn-success" href="{{route('mahasiswa.create')}}"> Input Mahasiswa</a>
+        </div>
+        <nav class="navbar navbar-light bg-light">
+            <form class="form-inline" action="{{ route('mahasiswa.cari') }}" method="GET">
+              <input class="form-control mr-sm-2" type="search" name="cari" placeholder="Cari mahasiswa.." aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </nav>
     </div>
+</div>
 
-    @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-    @endif
+@if ($message=Session::get('success'))
+<div class="alert alert-success">
+    <p>{{$message}}</p>
+</div>
+@endif
 
-    <table class="table table-bordered">
+<div class="table-responsive">
+<table class="table">
+   <tr>
+       <th>Nim</th>
+       <th>Nama</th>
+       <th>Foto</th>
+       <th>Kelas</th>
+       <th>Jurusan</th>
+       <th>Action</th>
+    </tr>
+
+    @foreach ($mhs as $Mahasiswa)
         <tr>
-            <th>Nim</th>
-            <th>Nama</th>
-            <th>Kelas</th>
-            <th>Jurusan</th>
-            <th>No_Handphone</th>
-            <th>Email</th>
-            <th>Tanggal Lahir</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($mahasiswa as $mhs)
-        <tr>
-            <td>{{ $mhs->Nim}}</td>
-            <td>{{ $mhs->Nama}}</td>
-            <td>{{ $mhs->kelas->nama_kelas}}</td>
-            <td>{{ $mhs->Jurusan }}</td>
-            <td>{{ $mhs->No_Handphone}}</td>
-            <td>{{ $mhs->EMail}}</td>
-            <td>{{ $mhs->Tanggal_lahir}}</td>
-            <td>
-                <form action="{{ route('mahasiswa.destroy',$mhs->id) }}" method="POST">
-                    <a class="btn btn-info" href="{{ route('mahasiswa.show',$mhs->id) }}">Show</a>
-                    <a class="btn btn-primary" href="{{ route('mahasiswa.edit',$mhs->id) }}">Edit</a>
+            <td>{{$Mahasiswa->Nim}}</td>
+            <td>{{$Mahasiswa->Nama}}</td> 
+            <td><img width="100px" height="100px" src="{{asset('storage/'.$Mahasiswa->foto)}}"></td>
+            <td>{{$Mahasiswa->kelas->nama_kelas}}</td>
+            <td>{{$Mahasiswa->Jurusan}}</td>
+            <td> 
+                <form action="{{route('mahasiswa.destroy',$Mahasiswa->id)}}" method="POST" onsubmit="return confirm('Anda yakin ingin menghapus data?')">
+                    <a class="btn btn-info" href="{{route('mahasiswa.tampil',$Mahasiswa->id)}}">Show</a>
+                    <a class="btn btn-primary" href="{{route('mahasiswa.edit',$Mahasiswa->id)}}">Edit</a>
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="btn btn-danger">Delete</button>
-                    <a class="btn btn-warning" href="{{route('mahasiswa.nilai',$mhs->id)}}">Nilai</a>
+                    <a class="btn btn-warning" href="{{route('mahasiswa.nilai',$Mahasiswa->id)}}">Nilai</a>
                 </form>
             </td>
         </tr>
     @endforeach
-    </table>
+</table>
+<div class="d-flex justify-content-center">
+    {{ $mhs->links() }}  
+</div>  
+</div>
 @endsection
