698747eaa26aea3da837262175a9227a619c288b
fajrin200 <muhammadfajrin412@gmail.com>
2021-05-02 11:04:22 +0700

    prak 3 - add package DomPDF

--- config/app.php
+++ config/app.php
@@ -174,7 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+         * Library tambahan...
+         */
+        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -227,7 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+         /*
+         * Library tambahan...
+         */
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
+
 
 ];
